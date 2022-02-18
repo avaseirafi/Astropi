c00fa7e7af587161b736678fff8d3e42ed7c2834
avaseirafi <73790268+avaseirafi@users.noreply.github.com>
2022-02-18 13:16:49 -0500

    defining the function of printing the location

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 #collect pictures of around the earth
 #sort them out 
+#do not change these
 from orbit import ISS
 from skyfield.api import load
 
-# Obtain the current time `t`
-t = load.timescale().now()
-# Compute where the ISS is at time `t`
-position = ISS.at(t)
-# Compute the coordinates of the Earth location directly beneath the ISS
-location = position.subpoint()
-print(location)
-
-print(f'Latitude: {location.latitude}')
-print(f'Longitude: {location.longitude}')
-print(f'Elevation: {location.elevation.km}')
-
-#
-print(f'Lat: {location.latitude.degrees:.1f}, Long: {location.longitude.degrees:.1f}')
+def print_iss_location():
+    # Obtain the current time `t`
+    t = load.timescale().now()
+    # Compute where the ISS is at time `t`
+    position = ISS.at(t)
+    # Compute the coordinates of the Earth location directly beneath the ISS
+    location = position.subpoint()
+    print(location)
+    print(f'Latitude: {location.latitude}')
+    print(f'Longitude: {location.longitude}')
+    print(f'Elevation: {location.elevation.km}')
+    print(f'Lat: {location.latitude.degrees:.1f}, Long: {location.longitude.degrees:.1f}')
 
 #this is when the camera is working
 from time import sleep
@@ -32,8 +31,80 @@ camera.start_preview()
 sleep(2)
 camera.capture(f"{base_folder}/image.jpg")
 
-#CPU tempera†ure
-from gpiozero import CPUTemperature
+#capturing the pictures and converting them to files we can open
+from orbit import ISS
+from picamera import PiCamera
+from pathlib import Path
 
-cpu = CPUTemperature()
-print(cpu.temperature)
+def convert(angle):
+    """
+    Convert a `skyfield` Angle to an EXIF-appropriate
+    representation (rationals)
+    e.g. 98° 34' 58.7 to "98/1,34/1,587/10"
+
+    Return a tuple containing a boolean and the converted angle,
+    with the boolean indicating if the angle is negative.
+    """
+    sign, degrees, minutes, seconds = angle.signed_dms()
+    exif_angle = f'{degrees:.0f}/1,{minutes:.0f}/1,{seconds*10:.0f}/10'
+    return sign < 0, exif_angle
+
+def capture(camera, image):
+    """Use `camera` to capture an `image` file with lat/long EXIF data."""
+    point = ISS.coordinates()
+
+    # Convert the latitude and longitude to EXIF-appropriate representations
+    south, exif_latitude = convert(point.latitude)
+    west, exif_longitude = convert(point.longitude)
+
+    # Set the EXIF tags specifying the current location
+    camera.exif_tags['GPS.GPSLatitude'] = exif_latitude
+    camera.exif_tags['GPS.GPSLatitudeRef'] = "S" if south else "N"
+    camera.exif_tags['GPS.GPSLongitude'] = exif_longitude
+    camera.exif_tags['GPS.GPSLongitudeRef'] = "W" if west else "E"
+
+    # Capture the image
+    camera.capture(image)
+
+cam = PiCamera()
+cam.resolution = (1296,972)
+
+base_folder = Path(__file__).parent.resolve()
+capture(cam, f"{base_folder}/gps1.jpg")
+
+#basically numbering plans for files
+from time import sleep
+from picamera import PiCamera
+from pathlib import Path
+
+base_folder = Path(__file__).parent.resolve()
+
+camera = PiCamera()
+camera.start_preview()
+sleep(2)
+for filename in camera.capture_continuous(f"{base_folder}/image_{counter:03d}.jpg"):
+    print(f'Captured {filename}')
+    sleep(300) # wait 5 minutes
+
+#creates a timelapse movie from the pictures taken in the code before
+ffmpeg -framerate 10 -i %*.jpg -c:v libx264 -crf 17 -pix_fmt yuv420p timelapse.mp4
+
+#fixes the resolution of the photos
+sudo raspi-config 
+
+#running our experiment for 3 hours
+from datetime import datetime, timedelta
+from time import sleep
+
+# Create a `datetime` variable to store the start time
+start_time = datetime.now()
+# Create a `datetime` variable to store the current time
+# (these will be almost the same at the start)
+now_time = datetime.now()
+# Run a loop for 2 minutes
+while (now_time < start_time + timedelta(minutes=2)):
+    print("Doing stuff")
+    sleep(1)
+    # Update the current time
+    now_time = datetime.now()
+
